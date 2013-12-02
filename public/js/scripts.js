d9ece7505cd7db51e42081a4427cf97e9d1d0c37
Marcin Wichary <mwichary@gmail.com>
2013-12-02 10:07:06 -0800

    Fix tooltip positioning.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -588,6 +588,8 @@ function hoverNeighborhoodEl(el, showTooltip) {
 
     var left = (boundingBox.x + boundingBox.width / 2 - hoverEl.offsetWidth / 2);
 
+    console.log(boundingBox.x, boundingBox.width, hoverEl.offsetWidth);
+
     hoverEl.style.top = top + 'px'; 
     hoverEl.style.left = left + 'px';
 
