4ca6f19a2ec26a444e275adf96460b761ced69ea
Joe Germuska <joe@germuska.com>
2014-01-02 12:50:20 -0600

    only use one congrats div WIP

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -15,7 +15,7 @@ var COUNTRY_NAME_WORLD = 'The World';
 var DEFAULT_NEIGHBORHOOD_NOUN_SINGULAR = 'neighborhood';
 var DEFAULT_NEIGHBORHOOD_NOUN_PLURAL = 'neighborhoods';
 
-var EASY_MODE_COUNT = 20;
+var EASY_MODE_COUNT = 2;
 
 var HIGHLIGHT_DELAY = 1500;
 var NEXT_GUESS_DELAY = 1000;
@@ -1225,7 +1225,7 @@ function updateTwitterLink(congratsEl) {
 }
 
 function gameOverPart2() {
-  var el = document.querySelector(easyMode ? '#congrats-easy' : '#congrats-hard');
+  var el = document.querySelector('#congrats');
 
   document.querySelector('#count-time-wrapper-wrapper').classList.remove('visible');
   document.querySelector('#more-cities-wrapper-wrapper').classList.add('visible');
