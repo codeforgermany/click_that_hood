59b085e7d15411a435d1a52998f10624a62e1a5c
Elaine Thomas <enajera2@cnm.edu>
2016-10-20 13:32:57 -0600

    second try with app.js from u/specious

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express'),
 	config = require('config')
 
 var engineLightStatusEndpoint = function(req, res) {
-
 	var response = {
 		'status': 'ok',
 		'updated': Math.round(Date.now() / 1000),
@@ -16,7 +15,6 @@ var engineLightStatusEndpoint = function(req, res) {
 	};
 
 	res.send(response);
-
 }
 
 function startApp() {
@@ -31,7 +29,7 @@ function startApp() {
 
 	// Redirect to environment-appropriate domain, if necessary
 	app.all('*', function(req, res, next) {
-		if(config.app_host_port != req.headers.host) {
+		if (config.app_host_port != req.headers.host) {
 			var redirectUrl = 'http://' + config.app_host_port + req.url;
 			console.log('Redirecting to ' + redirectUrl + '...');
 			res.redirect(301, redirectUrl);
@@ -49,347 +47,184 @@ function startApp() {
 
 	app.get('/', function(req, res) {
 		// redirect old-style /?location=XYZ and /?city=XYZ urls to new-style /XYZ
-		if(req.query.location) {
+		if (req.query.location) {
 			res.redirect(301, req.query.location);
-		} else if(req.query.city) {
+		} else if (req.query.city) {
 			res.redirect(301, req.query.city);
 		} else {
 			res.sendFile(__dirname + '/public/index.html');
 		}
 	});
 
-// 	app.use(express.static(__dirname + '/public'));
-//
-// 	app.listen(config.port, null, null, function() {
-// 		console.log('Listening on port ' + config.port);
-// 	});
-// }
-//
-// function normalizeCountryName(str) {
-// 	if(str.substr(0, 4) == 'The ') {
-// 		str = str.substr(4);
-// 	}
+	app.use(express.static(__dirname + '/public'));
+
+	app.listen(config.port, null, null, function() {
+		console.log('Listening on port ' + config.port);
+	});
+}
+
+function normalizeCountryName(str) {
+	if (str.substr(0, 4) == 'The ') {
+		str = str.substr(4);
+	}
 
 	return str;
 }
 
-+function getFeatureCoords(data) {
-	-
-	if(shapes[0] && shapes[0][0] && shapes[0][0][0] && shapes[0][0][0][0]) {
-		+
-		if(data[0][0]) {
-			-
-			return [shapes[0][0][0][0], shapes[0][0][0][1]] +
-			return getFeatureCoords(data[0])
-
-// function getSampleLatLon(shapes) {
-// 	if(shapes[0] && shapes[0][0] && shapes[0][0][0] && shapes[0][0][0][0]) {
-// 		return [shapes[0][0][0][0], shapes[0][0][0][1]]
-// 	} else if(shapes[0] && shapes[0][0] && shapes[0][0][0]) {
-// 		return [shapes[0][0][0], shapes[0][0][1]]
-// 	} else if(shapes[0] && shapes[0][0]) {
-// 		return [shapes[0][0], shapes[0][1]]
-// 	} else {
-// 		return [shapes[0], shapes[1]]
-// 	}
-// }
-
-		} else {
-			return [data[0], data[1]]
-				+
-		}
-		+
+function getFeatureCoords(data) {
+	if (data[0][0]) {
+		return getFeatureCoords(data[0])
+	} else {
+		return [data[0], data[1]]
 	}
-	+
-		+function checkFeature(locationName, feature, names, flags) {
-			+
-			var name = feature.properties.name
-				+ +
-			if(!name) {
-				+console.log('------------------------------------------------------')
-				+ console.log('Neighbourhood name missing in ' + locationName + '…')
-				+ console.log('Make sure the column with neighbourhood names is actually called “name.”')
-				+ process.exit(1)
-				+
-			}
-			+
-				+
-			if(name.match(/[a-z]/)) {
-				+flags.someLowercase = true
-					+
-			}
-			+
-				+
-			if(name.match(/[A-Z]/)) {
-				+flags.someUppercase = true
-					+
-			}
-			+
-				+
-			if(names[name]) {
-				+
-				var oldId = names[name].id
-					+
-				var newId = feature.properties.cartodb_id
-					+ +
-				if(!oldId && !newId) {
-					+oldId = 1
-					+ newId = 2
-						+
-				}
-				+
-					+console.log('------------------------------------------------------')
-				+ console.log('Name repetition (' + name + ') in ' + locationName + '…')
-				+ console.log(' ')
-				+ console.log('This is usually when a neighbourhood has a few disconnected/non overlapping polygons.')
-				+ console.log('These are two SQL commands that might unify two polygons into one.')
-				+ console.log(' ')
-				+ console.log('UPDATE ' + locationName + ' SET the_geom = ST_Union((SELECT the_geom FROM ' + locationName + ' WHERE cartodb_id = ' + oldId + '), (SELECT the_geom FROM ' + locationName + ' WHERE cartodb_id = ' + newId + ')) WHERE cartodb_id = ' + oldId + ';')
-				+ console.log('DELETE FROM ' + locationName + ' WHERE cartodb_id = ' + newId + ';')
-				+ +flags.errorOccurred = true
-					+
-			}
-			+
-				+names[name] = {id: feature.properties.cartodb_id}
-				+
-		}
-	+ +function isTemplateFile(file) {
-		+  // TODO: find a less obscure way to detect this file
-			+
-		return file.indexOf('/_') !== -1
-			+
-	}
-	+ +function readMetadataFile(file, metadata, countryNames) {
-		+
-		var locationName = path.basename(file, '.metadata.json');
-		+
-			+console.log('Reading metadata: ' + locationName)
-		+ +  // Flag error and exit if metadata is not found
-			+
-		if(!fs.existsSync(file)) {
-			+console.error('Metadata file not found for \'' + locationName + +'\'. Aborting server start.');
-			+process.exit(1);
-			+
-		}
-		+
-			+metadata[locationName] = JSON.parse(fs.readFileSync(file, 'utf8'));
-		+
-			+  // Combine a list of country names.
-				+
-		var countryName = metadata[locationName].countryName;
-		+
-			+
-		if(!countryName && !metadata[locationName].stateName) {
-			+countryName = 'The World';
-			+
-		}
-		+
-			+
-		if(countryName && countryNames.indexOf(countryName) === -1) {
-			+countryNames.push(countryName);
-			+
-		}
-		+
-			+  // Parse GeoJSON file, find the first available latitude/longitude,
-				+  // and add them to the metadata.
-					+
-		var geoJsonFilePath = 'public/data/' + locationName + '.geojson';
-		+
-			+
-		if(!fs.existsSync(geoJsonFilePath)) {
-			+console.error('GeoJSON file not found for \'' + locationName + +'\'. Aborting server start.');
-			+process.exit(1);
-			+
-		}
-		+
-			+
-		var geoJsonData = JSON.parse(fs.readFileSync(geoJsonFilePath, 'utf8'));
-		+
-			+  // Verify that names exist and that they don't repeat
-				+
-		var names = []
-			+
-		var flags = {
-			+ someLowercase
-		:
-		false,
-			+someUppercase
-		:
-		false,
-			+errorOccurred
-		:
-		false
-		+
-	}
-		+
-			+
-		for(var i in geoJsonData.features) {
-			+checkFeature(locationName, geoJsonData.features[i], names, flags)
-		}
+}
 
-		if(flags.errorOccurred) {
-			+process.exit(1)
-			+
-		}
-		+
-			+
-		if(!flags.someLowercase && flags.someUppercase) {
-			+console.log('------------------------------------------------------')
-			+ console.log('All neighbourhood names for ' + locationName + ' are uppercase…')
-			+ console.log(' ')
-			+ console.log('Try this SQL query to fix:')
-			+ console.log('UPDATE ' + locationName + ' SET name=initcap(lower(name));')
-			+ process.exit(1)
-			+
-		}
-		+
-			+
-		var latLon = getFeatureCoords(geoJsonData.features[0].geometry.coordinates)
-			+ +
-		if(latLon[0] === null || latLon[1] === null) {
-			+console.log('------------------------------------------------------')
-			+ console.log('WARNING: Cannot obtain average coordinates for ' + locationName + '…')
-			+
-		}
-		+
-			+metadata[locationName].sampleLatLon = latLon
+function checkFeature(locationName, feature, names, flags) {
+	var name = feature.properties.name
+
+	if (!name) {
+		console.log('------------------------------------------------------')
+		console.log('Neighbourhood name missing in ' + locationName + '…')
+		console.log('Make sure the column with neighbourhood names is actually called “name.”')
+		process.exit(1)
 	}
 
-	console.log('Initializing…')
+	if (name.match(/[a-z]/)) {
+		flags.someLowercase = true
+	}
+
+	if (name.match(/[A-Z]/)) {
+		flags.someUppercase = true
+	}
+
+	if (names[name]) {
+		var oldId = names[name].id
+		var newId = feature.properties.cartodb_id
+
+		if (!oldId && !newId) {
+			oldId = 1
+			newId = 2
+		}
+
+		console.log('------------------------------------------------------')
+		console.log('Name repetition (' + name + ') in ' + locationName + '…')
+		console.log(' ')
+		console.log('This is usually when a neighbourhood has a few disconnected/non overlapping polygons.')
+		console.log('These are two SQL commands that might unify two polygons into one.')
+		console.log(' ')
+		console.log('UPDATE ' + locationName + ' SET the_geom = ST_Union((SELECT the_geom FROM ' + locationName + ' WHERE cartodb_id = ' + oldId + '), (SELECT the_geom FROM ' + locationName + ' WHERE cartodb_id = ' + newId + ')) WHERE cartodb_id = ' + oldId + ';')
+		console.log('DELETE FROM ' + locationName + ' WHERE cartodb_id = ' + newId + ';')
+
+		flags.errorOccurred = true
+	}
+
+	names[name] = { id: feature.properties.cartodb_id }
+}
+
+function isTemplateFile(file) {
+	// TODO: find a less obscure way to detect this file
+	return file.indexOf('/_') !== -1
+}
+
+function readMetadataFile(file, metadata, countryNames) {
+	var locationName = path.basename(file, '.metadata.json');
+
+	console.log('Reading metadata: ' + locationName)
+
+	// Flag error and exit if metadata is not found
+	if (!fs.existsSync(file)) {
+		console.error('Metadata file not found for \'' + locationName +
+			'\'. Aborting server start.');
+		process.exit(1);
+	}
+
+	metadata[locationName] = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+	// Combine a list of country names.
+	var countryName = metadata[locationName].countryName;
+
+	if (!countryName && !metadata[locationName].stateName) {
+		countryName = 'The World';
+	}
+
+	if (countryName && countryNames.indexOf(countryName) === -1) {
+		countryNames.push(countryName);
+	}
+
+	// Parse GeoJSON file, find the first available latitude/longitude,
+	// and add them to the metadata.
+	var geoJsonFilePath = 'public/data/' + locationName + '.geojson';
+
+	if (!fs.existsSync(geoJsonFilePath)) {
+		console.error('GeoJSON file not found for \'' + locationName +
+			'\'. Aborting server start.');
+		process.exit(1);
+	}
+
+	var geoJsonData = JSON.parse(fs.readFileSync(geoJsonFilePath, 'utf8'));
+
+	// Verify that names exist and that they don't repeat
+	var names = []
+	var flags = {
+		someLowercase: false,
+		someUppercase: false,
+		errorOccurred: false
+	}
+
+	for (var i in geoJsonData.features) {
+		checkFeature(locationName, geoJsonData.features[i], names, flags)
+	}
+
+	if (flags.errorOccurred) {
+		process.exit(1)
+	}
+
+	if (!flags.someLowercase && flags.someUppercase) {
+		console.log('------------------------------------------------------')
+		console.log('All neighbourhood names for ' + locationName + ' are uppercase…')
+		console.log(' ')
+		console.log('Try this SQL query to fix:')
+		console.log('UPDATE ' + locationName + ' SET name=initcap(lower(name));')
+		process.exit(1)
+	}
+
+	var latLon = getFeatureCoords(geoJsonData.features[0].geometry.coordinates)
+
+	if (latLon[0] === null || latLon[1] === null) {
+		console.log('------------------------------------------------------')
+		console.log('WARNING: Cannot obtain average coordinates for ' + locationName + '…')
+	}
+
+	metadata[locationName].sampleLatLon = latLon
+}
+
+console.log('Initializing…')
 
 // Write combined metadata file from individual location metadata files
-	fsTools.findSorted('public/data', /[^.]+\.metadata.json/, function(err, files) {
+fsTools.findSorted('public/data', /[^.]+\.metadata.json/, function(err, files) {
+	var metadata = {};
+	var countryNames = ['U.S.'];
+	var totalNumFilesToUpload = 0;
+	var numFilesUploaded = 0;
 
-		var metadata = {};
-
-		var countryNames = ['U.S.'];
-
-		var totalNumFilesToUpload = 0;
-		var numFilesUploaded = 0;
-
-		for(var i in files) {
-			if(!isTemplateFile(files[i])) {
-				readMetadataFile(files[i], metadata, countryNames)
-
-				// Exclude template file
-// 		if(metadataFilePath.indexOf('/_') == -1) {
-// 			// Flag error and exit if metadata is not found
-// 			if(!fs.existsSync(metadataFilePath)) {
-// 				console.error('Metadata file not found for \'' + locationName +
-// 					'\'. Aborting server start.');
-// 				process.exit(1);
-// 			}
-//
-// 			metadata[locationName] =
-// 				JSON.parse(fs.readFileSync(metadataFilePath, 'utf8'));
-//
-// 			// Combine a list of country names.
-// 			var countryName = metadata[locationName].countryName;
-// 			if(!countryName && !metadata[locationName].stateName) {
-// 				countryName = 'The World';
-// 			}
-// 			if(countryName && countryNames.indexOf(countryName) == -1) {
-// 				countryNames.push(countryName);
-// 			}
-//
-//
-// 			// Parse GeoJSON file, find the first available latitude/longitude,
-// 			// and add them to the metadata.
-//
-// 			var geoJsonFilePath = 'public/data/' + locationName + '.geojson';
-// 			if(!fs.existsSync(geoJsonFilePath)) {
-// 				console.error('GeoJSON file not found for \'' + locationName +
-// 					'\'. Aborting server start.');
-// 				process.exit(1);
-// 			}
-//
-// 			var geoJsonData = JSON.parse(fs.readFileSync(geoJsonFilePath, 'utf8'));
-//
-// 			// Verify whether names exist, and also whether they don’t repeat
-// 			var names = []
-// 			var someLowercase = false
-// 			var someUppercase = false
-// 			var someErrors = false
-//
-// 			for(var i in geoJsonData.features) {
-// 				var data = geoJsonData.features[i]
-// 				var name = data.properties.name
-//
-// 				if(!name) {
-// 					console.log('------------------------------------------------------')
-// 					console.log('Name missing in ' + locationName + '…')
-// 					console.log('Make sure the column with neighbourhood names is actually called “name.”')
-// 					process.exit(1)
-// 				}
-//
-// 				if(name.match(/[a-z]/)) {
-// 					someLowercase = true
-// 				}
-// 				if(name.match(/[A-Z]/)) {
-// 					someUppercase = true
-// 				}
-//
-// 				if(names[name]) {
-// 					var oldId = names[name].id
-// 					var newId = data.properties.cartodb_id
-//
-// 					if(!oldId && !newId) {
-// 						oldId = 1
-// 						newId = 2
-// 					}
-//
-// 					console.log('------------------------------------------------------')
-// 					console.log('Name repetition (' + name + ') in ' + locationName + '…')
-// 					console.log(' ')
-// 					console.log('This is usually when a neighbourhood has a few disconnected/non overlapping polygons.')
-// 					console.log('These are two SQL commands that might unify two polygons into one.')
-// 					console.log(' ')
-// 					console.log('UPDATE ' + locationName + ' SET the_geom = ST_Union((SELECT the_geom FROM ' + locationName + ' WHERE cartodb_id = ' + oldId + '), (SELECT the_geom FROM ' + locationName + ' WHERE cartodb_id = ' + newId + ')) WHERE cartodb_id = ' + oldId + ';')
-// 					console.log('DELETE FROM ' + locationName + ' WHERE cartodb_id = ' + newId + ';')
-// 					someErrors = true
-// 				}
-//
-// 				names[name] = {id: data.properties.cartodb_id}
-// 			}
-//
-// 			if(someErrors) {
-// 				process.exit(1)
-// 			}
-//
-// 			if(!someLowercase && someUppercase) {
-// 				console.log('------------------------------------------------------')
-// 				console.log('All uppercase names for ' + locationName + '…')
-// 				console.log(' ')
-// 				console.log('Try this SQL query to fix:')
-// 				console.log('UPDATE ' + locationName + ' SET name=initcap(lower(name));')
-// 				process.exit(1)
-// 			}
-//
-// 			var latLon = geoJsonData.features[0].geometry.coordinates;
-// 			metadata[locationName].sampleLatLon = getSampleLatLon(latLon);
-//
-// 			if((metadata[locationName].sampleLatLon[0] == null) || (metadata[locationName].sampleLatLon[1] == null)) {
-// 				console.log('------------------------------------------------------')
-// 				console.log('WARNING: Unknown average location for ' + locationName + '…')
-// 			}
-// 		}
-// 	}
-//
-// 	countryNames.sort(function(a, b) {
-// 		return (normalizeCountryName(a) > normalizeCountryName(b)) ? 1 : -1;
-// 	});
-//
-// 	var metadataFileContents =
-// 		'//\n// This file is AUTO-GENERATED each time the ' +
-// 		'application is restarted.\n//\n\n' +
-// 		'var CITY_DATA = ' + JSON.stringify(metadata) + ';\n' +
-// 		'var COUNTRY_NAMES = ' + JSON.stringify(countryNames) + ';\n';
-// 	fs.writeFileSync('public/js/data.js', metadataFileContents);
-//
-// 	console.log('Done!')
-//
-// 	startApp();
-// });
-			}
-
+	for (var i in files) {
+		if (!isTemplateFile(files[i])) {
+			readMetadataFile(files[i], metadata, countryNames)
 		}
+	}
+
+	countryNames.sort(function(a, b) {
+		return (normalizeCountryName(a) > normalizeCountryName(b)) ? 1 : -1;
+	});
+
+	var metadataFileContents =
+		'//\n// This file is AUTO-GENERATED each time the ' +
+		'application is restarted.\n//\n\n' +
+		'var CITY_DATA = ' + JSON.stringify(metadata) + ';\n' +
+		'var COUNTRY_NAMES = ' + JSON.stringify(countryNames) + ';\n';
+	fs.writeFileSync('public/js/data.js', metadataFileContents);
+
+	console.log('Done!')
+
+	startApp();
+});
